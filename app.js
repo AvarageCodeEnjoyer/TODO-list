24594b5b1d56784ea4b2b93d74305481bb56a411
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2023-01-11 18:26:32 -0700

    added js

--- app.js
+++ app.js
@@ -1,63 +1,82 @@
-// Get references to the form and list elements
-const form = document.querySelectorAll("form")
+const form = document.querySelector("form")
 const list = document.getElementById("list")
 const submit = document.getElementById("submit")
+const clear = document.getElementById("clear")
+const help = document.getElementById("help")
+
+// var currentList = []
 // var listItems = JSON.parse(localStorage.getItem("list")) || []
 
-// Listen for form submissions
-// Listen for form submissions
-submit.addEventListener('click',  e => {
+
+
+/* -------------------------------------------------------------------------- */
+/*                               onclick events                               */
+/* -------------------------------------------------------------------------- */
+
+/* ------------ submit input to the list and update local storage ----------- */
+
+submit.addEventListener('click', e => {
   e.preventDefault();
-  
-  // Get the value of the input field
-  const input = document.getElementById("input").value;
+  let input = document.getElementById("input").value;
+  let newItem = document.createElement("li");
 
-  // Create a new li element
-  const newItem = document.createElement("li");
+  // Check if input is true
+  if(input === ""){
+    alert("Write Something")
+    return
+  }
 
-  // Add the input value as the text content of the new li element
   newItem.textContent = input;
-
-  // Add the new li element to the list
   list.appendChild(newItem);
-
-  // Get the current list items
-  const currentList = JSON.parse(localStorage.getItem("list")) || [];
-  
-  // Add the new item to the list
+  let currentList = JSON.parse(localStorage.getItem("list")) || [];
   currentList.push(input);
-
-  // Save the updated list to local storage
   localStorage.setItem("list", JSON.stringify(currentList));
 })
 
 
-// Retrieve the to-do list from local storage
-const savedList = JSON.parse(localStorage.getItem("list")) || [];
+/* ----------------------- 1 click on li for completed ---------------------- */
+// var ul = document.querySelector("ul")
+// var lis = list.querySelectorALL("li")
 
-savedList.forEach(item => {
-    // Create a new li element
-    const newItem = document.createElement("li");
-  
-    // Add the input value as the text content of the new li element
-    newItem.textContent = item;
-  
-    // Add the new li element to the list
-    list.appendChild(newItem);
+list.addEventListener('click', e => {
+  if (e.target.tagName === 'LI') {
+    // do something
+    console.log("hello")
+    e.target.classlist.add("completed")
+  }
+});
+
+/* --------------------- double click on li for removal --------------------- */
+
+list.addEventListener('dblclick', e => {
+  if (e.target.tagName === 'LI') {
+    // do something
+    console.log("hello")
+    list.removeChild(e.target)
+  }
 });
 
 
+/* --------------------- Clear local storage with button -------------------- */
 
-// Listen for clicks on the list items
-list.addEventListener("click", event => {
-  const target = event.target
-  
-  // If the click was on a list item
-  if (target.tagName === "LI") {
-    // Toggle the "completed" class on the list item
-    target.classList.toggle("completed")
-    
-    // Save the updated list to local storage
-    localStorage.setItem("list", list.innerHTML)
-  }
+clear.addEventListener('click', () => {
+  // e.preventDefault()
+  localStorage.clear()
 })
+
+/* -------------------------------------------------------------------------- */
+/*                                list display                                */
+/* -------------------------------------------------------------------------- */
+
+const savedList = JSON.parse(localStorage.getItem("list")) || [];
+
+savedList.forEach(item => {
+  //* Create a new li element
+  const newItem = document.createElement("li");
+
+  //* Add the input value as the text content of the new li element
+  newItem.textContent = item;
+
+  //* Add the new li element to the list
+  list.appendChild(newItem);
+});
